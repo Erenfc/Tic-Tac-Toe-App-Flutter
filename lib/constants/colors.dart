d8260d065330e9dc23c2d3c3de0ec58d747431c6
Syed Mohammed Safi S A <21cs111@psr.edu.in>
2024-04-21 18:23:07 +0530

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static Color? primaryColor = const Color.fromARGB(255, 39, 39, 39);
+  static Color secondaryColor = const Color.fromARGB(255, 194, 194, 194);
+  static Color? accentColor = Colors.red[400];
+}
